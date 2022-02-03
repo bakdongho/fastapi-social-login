c55c732ccf05089b745402405b6d5f4911e98b05
Park-DongHo <ehdgh4875@gmail.com>
2022-02-03 19:24:00 +0900

    Add provider parameters to each endpoint
    - 각 endpoint에 파라미터로 각 인스턴스를 생성할 수  있도록 변경

--- examples/tutorial.py
+++ examples/tutorial.py
@@ -1,8 +1,9 @@
+import secrets
 from typing import Optional
 import uvicorn
 import logging
 from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi import FastAPI, Request
+from fastapi import Depends, FastAPI, Query, Request
 from starlette.templating import Jinja2Templates
 from fastapi_oauth_client import OAuthClient
 
@@ -15,7 +16,7 @@ templates = Jinja2Templates(directory="templates")
 naver_client = OAuthClient(
     client_id="your_client_id",
     client_secret="your_client_secret_id",
-    redirect_uri="http://127.0.0.1:8000/callback/naver",
+    redirect_uri="http://127.0.0.1:8000/callback?provider=naver",
     authentication_url="https://nid.naver.com/oauth2.0",
     resource_url="https://openapi.naver.com/v1/nid/me",
 )
@@ -23,10 +24,16 @@ naver_client = OAuthClient(
 kakao_client = OAuthClient(
     client_id="your_client_id",
     client_secret="your_client_secret_id",
-    redirect_uri="http://127.0.0.1:8000/callback/kakao",
+    redirect_uri="http://127.0.0.1:8000/callback?provider=kakao",
     authentication_url="https://kauth.kakao.com/oauth",
     resource_url="https://kapi.kakao.com/v2/user/me",
 )
+
+def get_oauth_client(provider=Query(...,regex='naver|kakao')):
+    if provider == "naver":
+        return naver_client
+    elif provider == "kakao":
+        return kakao_client
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -34,29 +41,16 @@ async def login(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
 
-@app.get("/login/naver")
-async def login_naver():
-    login_url = naver_client.get_oauth_login_url(state="test")
+@app.get("/login")
+async def login_naver(oauth_client = Depends(get_oauth_client)):
+    state = secrets.token_urlsafe(32)
+    login_url = oauth_client.get_oauth_login_url(state=state)
     print(login_url)
     return RedirectResponse(login_url)
 
 
-@app.get("/login/kakao")
-async def login_kakao():
-    login_url = kakao_client.get_oauth_login_url(state="test")
-    print(login_url)
-    return RedirectResponse(login_url)
-
-
-@app.get("/callback/naver")
-async def callback(code: str, state: Optional[str] = None):
-    token_response = await naver_client.get_tokens(code, state)
-
-    return {"response": token_response}
-
-
-@app.get("/callback/kakao")
-async def callback(code: str, state: Optional[str] = None):
-    token_response = await kakao_client.get_tokens(code, state)
+@app.get("/callback")
+async def callback(code: str, state: Optional[str] = None, oauth_client = Depends(get_oauth_client)):
+    token_response = await oauth_client.get_tokens(code, state)
 
     return {"response": token_response}
