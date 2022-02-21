2461f6dc417e04f0936d568f738d09a87ac3e534
Park-DongHo <ehdgh4875@gmail.com>
2022-02-21 20:26:23 +0900

    Add endpoint
    1. user_info
    2. refresh token
    + 추가로 auth token 가져오는 함수 추가

--- examples/tutorial.py
+++ examples/tutorial.py
@@ -1,9 +1,8 @@
 import secrets
 from typing import Optional
-import uvicorn
-import logging
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi import Depends, FastAPI, Query, Request
+from fastapi import Depends, FastAPI, Header, Query, Request, HTTPException, status
 from starlette.templating import Jinja2Templates
 from fastapi_oauth_client import OAuthClient
 
@@ -15,25 +14,47 @@ templates = Jinja2Templates(directory="templates")
 
 naver_client = OAuthClient(
     client_id="your_client_id",
-    client_secret="your_client_secret_id",
-    redirect_uri="http://127.0.0.1:8000/callback?provider=naver",
-    authentication_url="https://nid.naver.com/oauth2.0",
-    resource_url="https://openapi.naver.com/v1/nid/me",
+    client_secret_id="your_client_secret_id",
+    redirect_uri="your_callback_uri",
+    authentication_uri="https://nid.naver.com/oauth2.0",
+    resource_uri="https://openapi.naver.com/v1/nid/me",
+    verify_uri="https://openapi.naver.com/v1/nid/verify",
 )
 
 kakao_client = OAuthClient(
     client_id="your_client_id",
-    client_secret="your_client_secret_id",
-    redirect_uri="http://127.0.0.1:8000/callback?provider=kakao",
-    authentication_url="https://kauth.kakao.com/oauth",
-    resource_url="https://kapi.kakao.com/v2/user/me",
+    client_secret_id="your_client_secret_id",
+    redirect_uri="your_callback_uri",
+    authentication_uri="https://kauth.kakao.com/oauth",
+    resource_uri="https://kapi.kakao.com/v2/user/me",
+    verify_uri="https://kapi.kakao.com/v1/user/access_token_info",
 )
 
-def get_oauth_client(provider=Query(...,regex='naver|kakao')):
+
+def get_oauth_client(provider: str = Query(..., regex="naver|kakao")):
     if provider == "naver":
         return naver_client
     elif provider == "kakao":
         return kakao_client
+
+
+def get_authorization_token(authorization: str = Header(...)) -> str:
+    scheme, _, param = authorization.partition(" ")
+    if not authorization or scheme.lower() != "bearer":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Not authenticated",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return param
+
+
+async def login_required(
+    oauth_client: OAuthClient = Depends(get_oauth_client),
+    access_token: str = Depends(get_authorization_token),
+):
+    if not await oauth_client.is_authenticated(access_token):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -42,15 +63,37 @@ async def login(request: Request):
 
 
 @app.get("/login")
-async def login_naver(oauth_client = Depends(get_oauth_client)):
+async def login_naver(oauth_client=Depends(get_oauth_client)):
     state = secrets.token_urlsafe(32)
     login_url = oauth_client.get_oauth_login_url(state=state)
-    print(login_url)
     return RedirectResponse(login_url)
 
 
 @app.get("/callback")
-async def callback(code: str, state: Optional[str] = None, oauth_client = Depends(get_oauth_client)):
+async def callback(
+    code: str, state: Optional[str] = None, oauth_client=Depends(get_oauth_client)
+):
     token_response = await oauth_client.get_tokens(code, state)
 
     return {"response": token_response}
+
+
+@app.get("/refresh")
+async def callback(
+    oauth_client=Depends(get_oauth_client),
+    refresh_token: str = Depends(get_authorization_token),
+):
+    token_response = await oauth_client.refresh_access_token(
+        refresh_token=refresh_token
+    )
+
+    return {"response": token_response}
+
+
+@app.get("/user", dependencies=[Depends(login_required)])
+async def get_user(
+    oauth_client=Depends(get_oauth_client),
+    access_token: str = Depends(get_authorization_token),
+):
+    user_info = await oauth_client.get_user_info(access_token=access_token)
+    return {"user": user_info}
