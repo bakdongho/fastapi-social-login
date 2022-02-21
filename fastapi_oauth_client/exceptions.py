2d3d07aff1d217c5f54decffe0a1eb929193d3be
Park-DongHo <ehdgh4875@gmail.com>
2022-02-21 20:25:09 +0900

    implement OauthClient method
    1. get_user_info
    2. refresh_access_token
    3. is_authenticated

--- fastapi_oauth_client/exceptions.py
+++ fastapi_oauth_client/exceptions.py
@@ -1,2 +1,5 @@
 class InvalidToken(Exception):
     pass
+
+class InvalidAuthorizationCode(Exception):
+    pass
--- fastapi_oauth_client/oauth_client.py
+++ fastapi_oauth_client/oauth_client.py
@@ -1,83 +1,102 @@
 from urllib import parse
-from typing import Optional
-from wsgiref import headers
-
+from typing import Dict, Optional
 import aiohttp
 import ssl
 import certifi
 
-from .exceptions import InvalidToken
+from .exceptions import InvalidAuthorizationCode, InvalidToken
 
 
 class OAuthClient:
     def __init__(
-        self, client_id, client_secret, redirect_uri, authentication_url, resource_url
+        self,
+        client_id,
+        client_secret_id,
+        redirect_uri,
+        authentication_uri,
+        resource_uri,
+        verify_uri,
     ):
         self._client_id = client_id
-        self._client_secret = client_secret
+        self._client_secret_id = client_secret_id
         self._redirect_uri = redirect_uri
-        self._authentication_url = authentication_url
-        self._resource_url = resource_url
+        self._authentication_uri = authentication_uri
+        self._resource_uri = resource_uri
+        self._verify_uri = verify_uri
         self._header_name = "Authorization"
         self._header_type = "Bearer"
 
-    def _get_connector_for_ssl(self):
+    def _get_connector_for_ssl(self) -> aiohttp.TCPConnector:
         ssl_context = ssl.create_default_context(cafile=certifi.where())
         return aiohttp.TCPConnector(ssl=ssl_context)
 
-    async def _request_for_token(self, payload):
+    async def _request_get_to(self, url, headers=None) -> Optional[Dict]:
         conn = self._get_connector_for_ssl()
         async with aiohttp.ClientSession(connector=conn) as session:
-            async with session.post(
-                f"{self._authentication_url}/token", data=payload
-            ) as resp:
-                return await resp.json()
+            async with session.get(url, headers=headers) as resp:
+                return None if resp.status != 200 else await resp.json()
 
-    def _get_payload_for_tokens(self, code: str, state: str):
-        return {
+    async def _request_post_to(self, url, payload=None) -> Optional[Dict]:
+        conn = self._get_connector_for_ssl()
+        async with aiohttp.ClientSession(connector=conn) as session:
+            async with session.post(url, data=payload) as resp:
+                return None if resp.status != 200 else await resp.json()
+
+    def get_oauth_login_url(self, state: str) -> str:
+        params = {
+            "response_type": "code",
             "client_id": self._client_id,
-            "client_secret": self._client_secret,
-            "grant_type": "authorization_code",
-            "code": code,
+            "redirect_uri": self._redirect_uri,
             "state": state,
         }
+        query_param = parse.urlencode(params, doseq=True)
 
-    def _get_headers_for_user_resource(self, access_token: str):
-        return {
-            self._header_name : f"{self._header_type} {access_token}"
-        }
+        return f"{self._authentication_uri}/authorize?{query_param}"
 
-    async def _request_for_token(self, access_token):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                self._resource_url, headers=self._get_headers_for_user_resource(access_token)
-            ) as resp:
-                return await resp.json()
+    async def get_tokens(self, code: str, state: str) -> Dict:
+        tokens = await self._request_post_to(
+            url=f"{self._authentication_uri}/token",
+            payload={
+                "client_id": self._client_id,
+                "client_secret": self._client_secret_id,
+                "grant_type": "authorization_code",
+                "code": code,
+                "state": state,
+            },
+        )
+        if tokens is None:
+            raise InvalidAuthorizationCode
 
-    async def _get_user_data(self, access_token: str):
-        pass
+        if tokens.get("access_token") is None or tokens.get("refresh_token") is None:
+            raise InvalidAuthorizationCode
 
-    async def get_user_data(self, access_token: str):
-        pass
+        return tokens
 
-    async def get_tokens(self, code: str, state: str):
-        payload = self._get_payload_for_tokens(code, state)
-        resp = await self._request_for_token(payload)
-        if resp.get("access_token") is None or resp.get("refresh_token") is None:
-            raise InvalidToken("Tokens can't be None")
+    async def refresh_access_token(self, refresh_token: str) -> Dict:
+        tokens = await self._request_post_to(
+            url=f"{self._authentication_uri}/token",
+            payload={
+                "client_id": self._client_id,
+                "client_secret": self._client_secret_id,
+                "grant_type": "refresh_token",
+                "refresh_token": refresh_token,
+            },
+        )
+        if tokens is None:
+            raise InvalidToken
+        return tokens
 
-        return resp
+    async def get_user_info(self, access_token: str) -> Dict:
+        headers = {self._header_name: f"{self._header_type} {access_token}"}
+        user_info = await self._request_get_to(url=self._resource_uri, headers=headers)
+        if user_info is None:
+            raise InvalidToken
+        return user_info
 
-    async def refresh_access_token(self, refresh_token: str):
-        pass
-
-    async def is_authenticated(self, access_token: str):
-        pass
-
-    def get_oauth_login_url(self, state: str):
-        client_id = f"client_id={self._client_id}"
-        redirect_uri = f"redirect_uri={parse.quote(self._redirect_uri, safe='')}"
-        response_type = "response_type=code"
-        state = f"state={state}"
-
-        return f"{self._authentication_url}/authorize?{client_id}&{redirect_uri}&{response_type}&{state}".strip()
+    async def is_authenticated(self, access_token: str) -> bool:
+        headers = {self._header_name: f"{self._header_type} {access_token}"}
+        res = await self._request_get_to(
+            url=self._verify_uri,
+            headers=headers,
+        )
+        return res is not None
